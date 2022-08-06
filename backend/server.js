267777de5ccc8499873c1ca8052072c98e8c1157
Eagles27 <maxime.gillen@outlook.fr>
2022-08-06 15:52:46 +0200

    feat: Connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
 // Create a new express application instance
@@ -17,7 +18,16 @@ app.use((req, res, next) => {
 // Routes
 app.use("/api/workouts", workoutRoutes);
 
-// listen on port 5000
-app.listen(process.env.PORT, () =>
-  console.log("Server started on port", process.env.PORT)
-);
+// Connect to the MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    // listen on port 5000
+    app.listen(process.env.PORT, () =>
+      console.log(
+        "Connected to DB and Server started on port",
+        process.env.PORT
+      )
+    );
+  })
+  .catch((err) => console.log(err));
