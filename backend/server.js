a5492413cc1476bc37094eefc0ff3430e838c5bb
Eagles27 <maxime.gillen@outlook.fr>
2022-08-06 15:32:35 +0200

    feat: Added Workouts Routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 // Create a new express application instance
 const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // Routes
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello World" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 // listen on port 5000
 app.listen(process.env.PORT, () =>
