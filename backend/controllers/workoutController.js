2ad26a6c0dd036f43c21a922dc62b6222c9bc646
Eagles27 <maxime.gillen@outlook.fr>
2022-08-06 17:16:04 +0200

    feat: Added all Routes, Backend V1 Finished

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,86 @@
+const Workout = require("../models/Workout");
+const mongoose = require("mongoose");
+
+// GET all workouts
+const getAllWorkouts = async (req, res) => {
+  try {
+    const workouts = await Workout.find().sort({ createdAt: -1 });
+    res.status(200).json(workouts);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// GET a single workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid ID" });
+  }
+
+  const workout = await Workout.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ error: "Workout not found" });
+  }
+
+  res.status(200).json(workout);
+};
+
+// CREATE a new workout
+const createWorkout = async (req, res) => {
+  const { title, reps, load } = req.body;
+
+  // ADD doc to collection
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// DELETE a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid ID" });
+  }
+  try {
+    const workout = await Workout.findByIdAndDelete(id);
+    if (!workout) {
+      return res.status(404).json({ error: "Workout not found" });
+    }
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// UPDATE a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid ID" });
+  }
+
+  try {
+    const workout = await Workout.findByIdAndUpdate(id, { ...req.body });
+    if (!workout) {
+      return res.status(404).json({ error: "Workout not found" });
+    }
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createWorkout,
+  getAllWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+};
