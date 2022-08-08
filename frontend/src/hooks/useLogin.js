bf838b21bc0b65c82f52626a11f741aff1099dc7
Eagles27 <maxime.gillen@outlook.fr>
2022-08-08 20:57:56 +0200

    feat: Added Login Hook

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const login = async (email, password) => {
+    setLoading(true);
+    setError(null);
+
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setLoading(false);
+      setError(json.error);
+    }
+
+    if (response.ok) {
+      // Save the token in local storage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      // Dispatch the login action
+      dispatch({ type: "LOGIN", payload: json });
+
+      setLoading(false);
+    }
+  };
+
+  return { login, loading, error };
+};
