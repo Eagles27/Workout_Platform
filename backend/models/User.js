338889fa8cb1ae88588c02ad5300bb8286932e3f
Eagles27 <maxime.gillen@outlook.fr>
2022-08-08 16:22:41 +0200

    feat: Added Hashed password Method in the User Schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -14,5 +15,23 @@ const UserSchema = new Schema({
     required: true,
   },
 });
+
+// Static method to authenticate user
+UserSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw new Error("User already exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    email,
+    password: hashedPassword,
+  });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", UserSchema);
